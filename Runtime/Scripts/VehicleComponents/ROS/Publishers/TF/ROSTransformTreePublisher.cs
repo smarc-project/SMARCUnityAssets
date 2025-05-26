bf1d9fe6b34b2a38fcea7f172842de3a75be2e00
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2025-05-26 10:58:47 +0200

    TFPub now sets its ori to 0 on validate. Teleporter now checks for frame_ids

--- Runtime/Scripts/VehicleComponents/ROS/Publishers/TF/ROSTransformTreePublisher.cs
+++ Runtime/Scripts/VehicleComponents/ROS/Publishers/TF/ROSTransformTreePublisher.cs
@@ -37,17 +37,24 @@ namespace VehicleComponents.ROS.Publishers
 
         void OnValidate()
         {
-            if(period < Time.fixedDeltaTime)
+            if (period < Time.fixedDeltaTime)
             {
-                Debug.LogWarning($"TF Publisher update frequency set to {Frequency}Hz but Unity updates physics at {1f/Time.fixedDeltaTime}Hz. Setting to Unity's fixedDeltaTime!");
-                Frequency = 1f/Time.fixedDeltaTime;
+                Debug.LogWarning($"TF Publisher update frequency set to {Frequency}Hz but Unity updates physics at {1f / Time.fixedDeltaTime}Hz. Setting to Unity's fixedDeltaTime!");
+                Frequency = 1f / Time.fixedDeltaTime;
             }
 
-            if(topic != "/tf")
+            if (topic != "/tf")
             {
                 Debug.LogWarning($"TF Publisher topic set to {topic} but should be /tf. Setting to /tf!");
                 topic = "/tf";
             }
+
+            if (transform.rotation != Quaternion.identity)
+            {
+                Debug.LogWarning($"[{transform.name}] TF Publisher transform (probably the root robot object: {transform.name}) is not identity, this will cause issues with the TF tree! Resetting to identity.");
+                transform.rotation = Quaternion.identity;
+            }
+            
         }
 
         protected override void StartROS()
@@ -106,8 +113,7 @@ namespace VehicleComponents.ROS.Publishers
 
             var mapToOdomMsg = new TransformMsg
             {
-                translation = OdomLinkGO.transform.To<ENU>().translation
-                // ignore orientation, so that odom and map are aligned always.
+                translation = OdomLinkGO.transform.To<ENU>().translation,
             };
             var mapToOdom = new TransformStampedMsg(
                 new HeaderMsg(new TimeStamp(Clock.time), $"map"),
@@ -115,10 +121,25 @@ namespace VehicleComponents.ROS.Publishers
                 mapToOdomMsg);
             tfMessageList.Add(mapToOdom);
 
+            // base_link is the robot's main frame, so we want to publish the transform from odom to base_link
+            var rosOdomPos = ENU.ConvertFromRUF(BaseLinkTreeNode.Transform.localPosition);
+            var rosOdomOri = ENU.ConvertFromRUF(BaseLinkTreeNode.Transform.localRotation);
+            var odomToBaseLinkMsg = new TransformMsg
+            {
+                translation = new Vector3Msg(
+                    rosOdomPos.x,
+                    rosOdomPos.y,
+                    rosOdomPos.z),
+                rotation = new QuaternionMsg(
+                    rosOdomOri.x,
+                    rosOdomOri.y,
+                    rosOdomOri.z,
+                    rosOdomOri.w)
+            };
             var odomToBaseLink = new TransformStampedMsg(
                 new HeaderMsg(new TimeStamp(Clock.time), $"{prefix}/odom"),
                 $"{prefix}/{BaseLinkTreeNode.name}",
-                BaseLinkTreeNode.Transform.To<ENU>());
+                odomToBaseLinkMsg);
             tfMessageList.Add(odomToBaseLink);
         }
 
