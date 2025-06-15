1bb75d00793272af8cb3014cd2d605185b63a03c
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2025-06-15 13:46:29 +0200

    Added search and follow tasks

--- Runtime/Scripts/SmarcGUI/MissionPlanning/Tasks/AlarsTasks.cs
+++ Runtime/Scripts/SmarcGUI/MissionPlanning/Tasks/AlarsTasks.cs
@@ -3,6 +3,31 @@ using SmarcGUI.MissionPlanning.Params;
 
 namespace SmarcGUI.MissionPlanning.Tasks
 {
+
+    public class AlarsSearch : Task
+    {
+        public override void SetParams()
+        {
+            Name = "alars-search";
+            Description = "Search for an AUV in the water";
+            Params.Add("search_center", new GeoPoint());
+            Params.Add("search_radius", 10.0f);
+        }
+    }
+
+
+    public class AlarsFollow : Task
+    {
+        public override void SetParams()
+        {
+            Name = "alars-follow";
+            Description = "Follow an AUV in the water";
+            Params.Add("timeout", 60);
+        }
+    }
+
+
+
     public class AlarsRecover : Task
     {
         public override void SetParams()
@@ -19,6 +44,8 @@ namespace SmarcGUI.MissionPlanning.Tasks
             Params.Add("raise_vertical", 10.0f);
         }
     }
+    
+    
 
     
 
