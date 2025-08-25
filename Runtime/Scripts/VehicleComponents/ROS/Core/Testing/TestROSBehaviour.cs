1ecefacb1c74297284e76dc7328dc0ca3791d8d8
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2025-08-25 16:48:11 +0200

    Working example of a proper pub in testRosBehaviour

---
+++ Runtime/Scripts/VehicleComponents/ROS/Core/Testing/TestROSBehaviour.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using Unity.Robotics.Core;
+using VehicleComponents.ROS.Core;
+using RosMessageTypes.Std;
+using DefaultNamespace.Water;
+using DefaultNamespace;
+
+
+public class TestROSBehaviour : ROSBehaviour
+{
+    Float64Msg ROSMsg;
+    [Header("ROS Publisher")]
+    public float frequency = 10f;
+    float period => 1f/frequency;
+    double lastPublished = 0f;
+    int i = 0;
+
+
+    protected override void StartROS()
+    {
+        rosCon.RegisterPublisher<Float64Msg>(topic);
+        ROSMsg = new Float64Msg();
+        lastPublished = Clock.Now;
+    }
+
+
+    void FixedUpdate()
+    {
+
+        double now = Clock.Now;
+        double timeSinceLastPub = now - lastPublished;
+
+        while (timeSinceLastPub >= period)
+        {
+            ROSMsg.data = ++i;
+            rosCon.Publish(topic, ROSMsg);
+            lastPublished += period;
+            timeSinceLastPub = now - lastPublished;
+        }
+        
+
+        
+    }
+}
